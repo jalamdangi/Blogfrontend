5bc5b1a927e4ffaf7ff6c5319a5fbb6eef514371
jalamdangi <jalamdangi96@gmail.com>
2022-04-05 12:13:24 +0530

    some updates added

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import Singleblog from './pages/Singleblog';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Check from './pages/Check';
+import Errorpage from './pages/Errorpage';
+import Fetchbytags from './pages/Fetchbytags';
+import Userprofile from './pages/Userprofile';
+// import Singeblog from './pages/Singleblog';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/blog' element={<Blog/>}/>
+          <Route exact path='/contact' element={<Contact/>}/>
+          <Route exact path='blog/singleblog/:id' element={<Singleblog/>}/>
+          <Route exact path='/login' element={<Signin/>}/>
+          <Route exact path='/signup' element={<Signup/>}/>
+          <Route exact path='/check' element={<Check/>}/>
+          <Route exact path='/fetchbytags/:tag' element={<Fetchbytags/>}/>
+          <Route exact path="/fetchbytags/:tags/singleblog/:id" element={<Singleblog />}></Route>
+          <Route exact path='/userprofile' element={<Userprofile/>}/>
+          <Route path="*" element={<Errorpage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Userprofile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Userprofile = () => {
+  return (
+    <>
+        <div>
+            <h2>Welcome to your world..</h2>
+        </div>
+    </>
+  )
+}
+
+export default Userprofile
